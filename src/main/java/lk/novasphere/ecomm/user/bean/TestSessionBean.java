6ad709f25e78abadb2309d5ce1e59b8955e4cd19
Maleesha Hirushan <maleeshahir@gmail.com>
2026-06-07 12:22:35 +0530

    Day 19 | End

--- src/main/java/lk/novasphere/ecomm/user/bean/TestSessionBean.java
+++ src/main/java/lk/novasphere/ecomm/user/bean/TestSessionBean.java
@@ -2,15 +2,14 @@ package lk.novasphere.ecomm.user.bean;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
-import jakarta.ejb.PostActivate;
-import jakarta.ejb.PrePassivate;
-import jakarta.ejb.Stateful;
+import jakarta.ejb.*;
 import lk.novasphere.ecomm.user.remote.TestRemote;
 
 @Stateful
 public class TestSessionBean implements TestRemote {
 
     int i;
+
 
     @PostConstruct
     public void init() {
@@ -22,32 +21,28 @@ public class TestSessionBean implements TestRemote {
         System.out.println("TestSessionBean : destroy");
     }
 
+
     @PostActivate
-    public void postActivate(){
+    public void postActivate() {
         System.out.println("TestSessionBean : postActivate");
     }
 
     @PrePassivate
-    public void prePassivate(){
+    public void prePassivate() {
         System.out.println("TestSessionBean : prePassivate");
     }
 
 
-//    public TestSessionBean() {
-//        System.out.println("TestSessionBean Created" + this);
-//    }
-
     @Override
     public String test() {
-
         i++;
 
-//        try {
-//            Thread.sleep(5000);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
-        return ("TestSessionBean : test" + i);
+        return "TestSessionBean : test"+i;
     }
 }
